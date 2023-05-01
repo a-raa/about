d189bdc21cf3465c594653a82102f11817e5780e
조아라 <rovhrud21@naver.com>
2023-05-02 05:00:01 +0900

    firsdddagt commit

--- app/page.js
+++ app/page.js
@@ -8,40 +8,40 @@ export default function Home() {
   const isDesktopOrMobile = useMediaQuery({ query: '(max-width:920px)' });
   const isDesktopOrMobile2 = useMediaQuery({ query: '(max-width:500px)' });
   return (
-      <>
-      
-        {isDesktopOrMobile !== true ?
-          <div className="content mainC">
+    <>
+
+      {isDesktopOrMobile !== true ?
+        <div className="content mainC">
+          <div className="content_t">
+            <div className="bestText">
+              <h2>어쩌구저쩌구</h2>
+              <h2>어쩌구저쩌구</h2>
+            </div>
+          </div>
+        </div>
+        :
+        // width 920
+        isDesktopOrMobile2 !== true ?
+          <div className="content mainC tMainC">
             <div className="content_t">
               <div className="bestText">
-                <h2>BEST#DEVELOPER</h2>
-                <h2>BEST#DEVELOPER</h2>
+                <h2>어쩌구저쩌구</h2>
+                <h2>어쩌구저쩌구</h2>
               </div>
             </div>
           </div>
           :
-          // width 920
-          isDesktopOrMobile2 !== true ?
-            <div className="content mainC tMainC">
-              <div className="content_t">
-                <div className="bestText">
-                  <h2>어쩌구저쩌구</h2>
-                  <h2>어쩌구저쩌구</h2>
-                </div>
+          //width 500
+          <div className="content mainC mMainC">
+            <div className="content_t">
+              <div className="bestText">
+                <h2>어쩌구<br />저쩌구</h2>
+                <h2>어쩌구<br />저쩌구</h2>
               </div>
             </div>
-            :
-            //width 500
-            <div className="content mainC mMainC">
-              <div className="content_t">
-                <div className="bestText">
-                  <h2>어쩌구<br />저쩌구</h2>
-                  <h2>어쩌구<br />저쩌구</h2>
-                </div>
-              </div>
-            </div>
-        }
-      </>
+          </div>
+      }
+    </>
   )
 }
 
